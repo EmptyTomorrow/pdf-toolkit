338019b386f016d88a9f70cfc1689d173dae7c2e
mineski1337 <mineski1337@gmail.com>
2022-06-28 12:11:55 +0900

    расширил параметры

--- app.py
+++ app.py
@@ -8,22 +8,29 @@ flask_app = Flask("PDF Toolkit")
 @flask_app.route('/addstamp', methods = ["POST"])
 def add_stamp():
 
+    # Получаем параметры 
+    # TODO дефолтные значения
     pagenum = int(request.args.get('pageNum'))
-    x = int(request.args.get('x'))
-    y = int(request.args.get('y'))
+    x0 = int(request.args.get('x0'))
+    y0 = int(request.args.get('y0'))
+    x1 = int(request.args.get('x1'))
+    y1 = int(request.args.get('y1'))
     inputFile = request.files['inputFile']
     stampFile = request.files['stampFile']
 
+    # Открываем файл из стрима и страницы
+    # TODO проверки
     file_handle = fitz.open(stream=inputFile.stream.read())
-    page_handle = file_handle[pagenum]
+    page_handle = file_handle[pagenum - 1]
 
-    stamp_rectangle = fitz.Rect(0, 0, x, y)
+    stamp_rectangle = fitz.Rect(x0, y0, x1, y1)
 
+    # Перед вставкой картинки нормализуем страницу
     if not page_handle.is_wrapped: 
         page_handle.wrap_contents()
 
     page_handle.insert_image(stamp_rectangle, stream=stampFile.stream.read())
-
+    # Возвращаем в виде байт, можно файлы и в base64 передавать вместо бинарей
     file_bytes = file_handle.tobytes()
     return send_file(io.BytesIO(file_bytes), mimetype="application/pdf", as_attachment=False)
 
